20a07c986d2bddf47e2509a2388d2fc84872ba41
Robert Mixon <mixonroberth@gmail.com>
2023-12-13 13:15:23 -0700

    basic test with pytest

--- src/pages/homepage.py
+++ src/pages/homepage.py
@@ -1,17 +1,12 @@
 from seleniumpagefactory.Pagefactory import PageFactory
 
+
+def check_page_loaded(): None
+
+
 class Homepage(PageFactory):
-    def __init__(self, driver):
-        self.driver = driver
-
-    locators = {
-    "sign_in": ("ID", "signin"),
-    "user_name": ("CSS", ".username")
-    }
-
-    def click_sign_in(self):
-        self.sign_in.click()
-
-    def get_username(self):
-        retrieved_username = self.user_name.get_text()
-        assert retrieved_username == "demouser"
+        def __init__(self, driver):
+            self.driver = driver
+            
+        locators = {}
+            
